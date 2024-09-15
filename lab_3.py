83cd7b6eac688666ef7b619df8783d21afc1759b
Zipeng Fu <zipengfu@outlook.com>
2024-09-15 03:35:21 -0700

    init

---
+++ lab_3.py
@@ -0,0 +1,150 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import JointState
+from std_msgs.msg import Float64MultiArray
+import numpy as np
+np.set_printoptions(precision=3, suppress=True)
+
+Kp = 3
+Kd = 0.1
+
+class InverseKinematics(Node):
+
+    def __init__(self):
+        super().__init__('inverse_kinematics')
+        self.joint_subscription = self.create_subscription(
+            JointState,
+            'joint_states',
+            self.listener_callback,
+            10)
+        self.joint_subscription  # prevent unused variable warning
+
+        self.command_publisher = self.create_publisher(
+            Float64MultiArray,
+            '/forward_command_controller/commands',
+            10
+        )
+
+        self.pd_timer_period = 1.0 / 200  # 200 Hz
+        self.ik_timer_period = 1.0 / 20   # 10 Hz
+        self.pd_timer = self.create_timer(self.pd_timer_period, self.pd_timer_callback)
+        self.ik_timer = self.create_timer(self.ik_timer_period, self.ik_timer_callback)
+
+        self.joint_positions = None
+        self.joint_velocities = None
+        self.target_joint_positions = None
+
+        self.ee_triangle_positions = np.array([
+            [0.05, 0.0, -0.12],  # Touchdown
+            [-0.05, 0.0, -0.12], # Liftoff
+            [0.0, 0.0, -0.06]    # Mid-swing
+        ])
+
+        center_to_rf_hip = np.array([0.07500, -0.08350, 0])
+        self.ee_triangle_positions = self.ee_triangle_positions + center_to_rf_hip
+        self.current_target = 0
+        self.t = 0
+
+    def listener_callback(self, msg):
+        joints_of_interest = ['leg_front_r_1', 'leg_front_r_2', 'leg_front_r_3']
+        self.joint_positions = np.array([msg.position[msg.name.index(joint)] for joint in joints_of_interest])
+        self.joint_velocities = np.array([msg.velocity[msg.name.index(joint)] for joint in joints_of_interest])
+
+    def forward_kinematics(self, theta1, theta2, theta3):
+        ################################################################################################
+        # TODO: paste lab 2 forward kinematics here
+        ################################################################################################
+        return
+
+    def inverse_kinematics(self, target_ee, initial_guess=[0, 0, 0]):
+        def cost_function(theta):
+            # Compute the cost function and the L1 norm of the error
+            # return the cost and the L1 norm of the error
+            ################################################################################################
+            # TODO: Implement the cost function
+            ################################################################################################
+            return None, None
+
+        def gradient(theta, epsilon=1e-3):
+            # Compute the gradient of the cost function using finite differences
+            ################################################################################################
+            # TODO: Implement the gradient computation
+            ################################################################################################
+            return
+
+        theta = np.array(initial_guess)
+        learning_rate = None # TODO: Set the learning rate
+        max_iterations = None # TODO: Set the maximum number of iterations
+        tolerance = None # TODO: Set the tolerance for the L1 norm of the error
+
+        cost_l = []
+        for _ in range(max_iterations):
+            grad = gradient(theta)
+
+            # Update the theta (parameters) using the gradient and the learning rate
+            ################################################################################################
+            # TODO: Implement the gradient update
+            # TODO (BONUS): Implement the (quasi-)Newton's method for faster convergence
+            theta -= None
+            ################################################################################################
+
+            cost, l1 = cost_function(theta)
+            # cost_l.append(cost)
+            if l1.mean() < tolerance:
+                break
+
+        # print(f'Cost: {cost_l}')
+
+        return theta
+
+    def interpolate_triangle(self, t):
+        # Intepolate between the three triangle positions in the self.ee_triangle_positions
+        # based on the current time t
+        ################################################################################################
+        # TODO: Implement the interpolation function
+        ################################################################################################
+        return
+
+    def ik_timer_callback(self):
+        if self.joint_positions is not None:
+            target_ee = self.interpolate_triangle(self.t)
+            self.target_joint_positions = self.inverse_kinematics(target_ee, self.joint_positions)
+            current_ee = self.forward_kinematics(*self.joint_positions)
+
+            # update the current time for the triangle interpolation
+            ################################################################################################
+            # TODO: Implement the time update
+            ################################################################################################
+            
+            self.get_logger().info(f'Target EE: {target_ee}, Current EE: {current_ee}, Target Angles: {self.target_joint_positions}, Target Angles to EE: {self.forward_kinematics(*self.target_joint_positions)}, Current Angles: {self.joint_positions}')
+
+    def pd_timer_callback(self):
+        if self.joint_positions is not None and self.joint_velocities is not None and self.target_joint_positions is not None:
+            ################################################################################################
+            torques = None
+            # TODO: paste lab 1 pd controller here 
+            ################################################################################################
+
+            command_msg = Float64MultiArray()
+            command_msg.data = torques.tolist()
+            self.command_publisher.publish(command_msg)
+
+def main():
+    rclpy.init()
+    inverse_kinematics = InverseKinematics()
+    
+    try:
+        rclpy.spin(inverse_kinematics)
+    except KeyboardInterrupt:
+        print("Program terminated by user")
+    finally:
+        # Send zero torques
+        zero_torques = Float64MultiArray()
+        zero_torques.data = [0.0, 0.0, 0.0]
+        inverse_kinematics.command_publisher.publish(zero_torques)
+        
+        inverse_kinematics.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
