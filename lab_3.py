6f61f909c59528146daf8ad77cb6e626bc5392ea
stong88 <70980554+stong88@users.noreply.github.com>
2025-10-06 17:14:21 -0700

    fix array dimensions on interpolate

--- lab_3.py
+++ lab_3.py
@@ -169,7 +169,7 @@ class InverseKinematics(Node):
         # TODO: Implement the interpolation function
         ################################################################################################
         timesteps = [0, 1, 2, 3]
-        ee_triangle_positions_with_end = np.append(self.ee_triangle_positions, self.ee_triangle_positions[0, :])
+        ee_triangle_positions_with_end = np.append(self.ee_triangle_positions, [self.ee_triangle_positions[0, :]])
 
         x = np.interp(t, timesteps, ee_triangle_positions_with_end[:, 0])
         y = np.interp(t, timesteps, ee_triangle_positions_with_end[:, 1])
